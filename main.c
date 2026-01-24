ed74131f488b43d400703921ec751026d6d38438
AhmadAL-Quraan <ahmadquraan.git@gmail.com>
2026-01-24 23:26:49 +0300

    Finishing printf

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
 #include "ft_printf.h"
 
-int main() { ft_printf("%s", "ahmad"); }
+int main() {
+  ft_printf("%s", "ahmad");
+  ft_printf("%c", 'd');
+  ft_printf("%d", 21);
+  ft_printf("%s", NULL);
+  ft_printf("%p", NULL);
+}
